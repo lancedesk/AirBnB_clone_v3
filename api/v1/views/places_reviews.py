d0ff0a407b3ad3f13d3f88928895437e5ec96fe0
Lance Desk <lncedesk@gmail.com>
2024-02-24 03:30:47 -0800

    Review endpoints for managing review objects.

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -4,89 +4,109 @@ Review endpoints for managing review objects.
 """
 
 from models import storage
+from models.user import User
+from models.place import Place
 from models.review import Review
 from api.v1.views import app_views
-from flask import abort, jsonify, make_response, request
+from flask import jsonify, request, abort
 
 
-@app_views.route("/places/<string:place_id>/reviews", methods=["GET"])
+@app_views.route("/places/<place_id>/reviews")
 def get_reviews(place_id):
     """
     Retrieve all reviews for a specific place.
     """
-    place = storage.get("Place", place_id)
-    if place is None:
+    place = storage.get(Place, place_id)
+
+    if not place:
         abort(404)
-    reviews = [review.to_dict() for review in place.reviews]
-    return make_response(jsonify(reviews), 200)
+
+    result = []
+
+    for review in place.reviews:
+        result.append(review.to_dict())
+
+    return jsonify(result)
 
 
-@app_views.route("/reviews/<string:review_id>", methods=["GET"])
+@app_views.route("/reviews/<review_id>")
 def get_review(review_id):
     """
     Retrieve a specific review by ID.
     """
-    review = storage.get("Review", review_id)
-    if review is None:
+    review = storage.get(Review, review_id)
+
+    if not review:
         abort(404)
-    return make_response(jsonify(review.to_dict()), 200)
+
+    return jsonify(review.to_dict())
 
 
-@app_views.route("/reviews/<string:review_id>", methods=["DELETE"])
+@app_views.route("/reviews/<review_id>", methods=["DELETE"])
 def delete_review(review_id):
     """
     Delete a review by ID.
     """
-    review = storage.get("Review", review_id)
-    if review is None:
+    review = storage.get(Review, review_id)
+
+    if not review:
         abort(404)
+
     review.delete()
     storage.save()
-    return make_response(jsonify({}), 200)
+
+    return jsonify({})
 
 
-@app_views.route("/places/<string:place_id>/reviews", methods=["POST"])
+@app_views.route("/places/<place_id>/reviews", methods=["POST"])
 def create_review(place_id):
     """
     Create a new review for a specific place.
     """
-    place = storage.get("Place", place_id)
-    if place is None:
+    place = storage.get(Place, place_id)
+    payload = request.get_json()
+
+    if not place:
         abort(404)
-    if not request.get_json():
+    if not payload:
         abort(400, "Not a JSON")
-    kwargs = request.get_json()
-    if "user_id" not in kwargs:
+    if "user_id" not in payload:
         abort(400, "Missing user_id")
-    user = storage.get("User", kwargs["user_id"])
-    if user is None:
+    if not storage.get(User, payload["user_id"]):
         abort(404)
-    if "text" not in kwargs:
+    if "text" not in payload:
         abort(400, "Missing text")
-    kwargs["place_id"] = place_id
-    review = Review(**kwargs)
+
+    review = Review(place_id=place_id, **payload)
     review.save()
-    return make_response(jsonify(review.to_dict()), 201)
+
+    return jsonify(review.to_dict()), 201
 
 
-@app_views.route("/reviews/<string:review_id>", methods=["PUT"])
+@app_views.route("/reviews/<review_id>", methods=["PUT"])
 def update_review(review_id):
     """
     Update a review by ID.
     """
-    review = storage.get("Review", review_id)
-    if review is None:
+    review = storage.get(Review, review_id)
+    payload = request.get_json()
+
+    if not review:
         abort(404)
-    if not request.get_json():
+
+    if not payload:
         abort(400, "Not a JSON")
-    for attr, val in request.get_json().items():
-        if attr not in [
+
+    for key, value in review.to_dict().items():
+        if key not in [
             "id",
             "user_id",
             "place_id",
             "created_at",
             "updated_at",
+            "__class__",
         ]:
-            setattr(review, attr, val)
+            setattr(review, key, payload[key] if key in payload else value)
     review.save()
-    return make_response(jsonify(review.to_dict()), 200)
+
+    return jsonify(review.to_dict())
