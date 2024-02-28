900cf21d5562268d69fc6dbcaea46672ceb0a9a6
Lance Desk <lncedesk@gmail.com>
2024-02-27 18:42:56 -0800

    Modifications to init and module files.

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,6 +4,8 @@ This module initializes blueprint of the API.
 """
 from flask import Blueprint
 
+app_views = Blueprint("app_views", __name__)
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -12,5 +14,3 @@ from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
-
-app_views = Blueprint("app_views", __name__)
